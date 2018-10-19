ba2f7faa57f83a03c08e91f3d3997705c6050afc
kminchul <k.minchul95@gmail.com>
2018-10-20 02:22:19 +0900

    P13. changed indentation

--- Math/Problem13/main.cpp
+++ Math/Problem13/main.cpp
@@ -4,22 +4,22 @@ using namespace std;
 
 double Problem13(int n)
 {
-	int cnt=0;
+    int cnt=0;
+
+    for(int i=1;i<2*n;i+=2)
+    {
+        for(int j=1;j<2*n;j+=2)
+        {
+            if(i*i + j*j < 4*n*n)
+                cnt++;
+        }
+    }
  
-	for(int i=1;i<2*n;i+=2)
-	{
-		for(int j=1;j<2*n;j+=2)
-		{
-			if(i*i + j*j < 4*n*n)
-				cnt++;
-		}
-	}
- 
-	return (4.0*cnt)/(n*n);
+    return (4.0*cnt)/(n*n);
 }
  
 int main(int argc, char* argv[])
 {
-	cout << Problem13(1000) << endl;
+    cout << Problem13(1000) << endl;
     return 0;
 }
