551cef63c8fb75063e3090bbcfdd0f824880cea4
kminchul <k.minchul95@gmail.com>
2018-10-20 02:21:08 +0900

    P10. changed indentation

--- Math/Problem10/main.cpp
+++ Math/Problem10/main.cpp
@@ -3,27 +3,26 @@
 #include <bitset>
 
 using namespace std;
- 
+
 void Problem10(int bits)
 {
-	vector<int> gcode;
-	vector<int> inv_gcode(1<<bits);
-	
-	for(int i=0;i<(1<<bits);i++)
-		gcode.push_back(i ^ (i>>1));
-	
-	for(int i=0;i<gcode.size();i++)
-		inv_gcode[gcode[i]] = i;
-	
-	for(int i=0;i<gcode.size();i++)
-		cout << i << " " << bitset<5>(gcode[i]) << " " << bitset<5>(inv_gcode[gcode[i]]) << endl;
-		
-	return;
+    vector<int> gcode;
+    vector<int> inv_gcode(1<<bits);
+
+    for(int i=0;i<(1<<bits);i++)
+        gcode.push_back(i ^ (i>>1));
+
+    for(int i=0;i<gcode.size();i++)
+        inv_gcode[gcode[i]] = i;
+
+    for(int i=0;i<gcode.size();i++)
+        cout << i << " " << bitset<5>(gcode[i]) << " " << bitset<5>(inv_gcode[gcode[i]]) << endl;
+
+    return;
 }
  
 int main(int argc, char* argv[])
 {
-	Problem10(5);
-	
+    Problem10(5);
     return 0;
 }
