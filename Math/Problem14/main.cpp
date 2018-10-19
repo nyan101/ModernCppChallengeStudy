0a514d3f56dfa2a01e25713950d46beb6b85eea7
kminchul <k.minchul95@gmail.com>
2018-10-20 02:19:50 +0900

    Solved Problem 14

--- Math/Problem14/main.cpp
+++ Math/Problem14/main.cpp
@@ -1,6 +1,27 @@
-#include <gsl/gsl>
+#include <iostream>
+#include <string>
 
+using namespace std;
+ 
+bool Problem14(string ISBN)
+{
+    if(ISBN.length()!=10)
+        return false;
+
+    int chksum = 0;
+    for(int i=0;i<=8;i++)
+        chksum += (ISBN.at(i)-'0') * (10-i);
+
+    chksum %= 11;
+
+    if(chksum==1)
+        return ISBN.at(9)=='X';
+
+    return (ISBN.at(9)-'0' + chksum)%11==0;
+}
+ 
 int main(int argc, char* argv[])
 {
+    cout << Problem14("0198526636") << endl;
     return 0;
 }
